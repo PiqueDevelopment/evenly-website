2fd65225bc72c5be7715a18471f20282b70c5249
dliu04 <dliuninja@gmail.com>
2024-12-26 11:43:31 -0800

    feat Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        Evenly
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.ts';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
