352f63d749662a4e95c06cc70f6082b996e5b56e
dliu04 <dliuninja@gmail.com>
2024-12-26 12:23:51 -0800

    feat Navbar CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.tsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Docs from './pages/Docs';
+// import About from './pages/About';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Evenly</h1>
       <Navbar />
+      <Routes>
+        <Route path="/"/>
+        {/* <Route path="/docs" element={<Docs />} />
+        <Route path="/about" element={<About />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,53 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { Button } from './Button.tsx';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton); // Shows the button whenever the screen is resized
+
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        Evenly
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        Evenly <i className='fab fa-typo3'></i>
                     </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/docs' className='nav-links' onClick={closeMobileMenu}>
+                                Docs
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>ABOUT</Button>}
                 </div>
             </nav>
         </>
