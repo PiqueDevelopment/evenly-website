ff46ac29e93c079c504f958e4f5bd6364844a42a
dliu04 <dliuninja@gmail.com>
2024-12-27 12:34:07 -0800

    feat Home page wip

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,11 @@ function Hero() {
   return (
     <div className='hero-container'>
         {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
-        <h1>Bill splitting, done simply.</h1>
-        <p>Download today!</p>
+        <h1>Evenly</h1>
+        <h2>Bill splitting, done simply.</h2>
+        <p>
+          Split bills, track expenses, settle balances - all natively within your Google Sheets.
+        </p>
         <div className='hero-btns'>
             <Button to='/docs' className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get Started</Button>
             <Button to='/download' className='btns' buttonStyle='btn--download' buttonSize='btn--large'>Download <i className="fa fa-download" /> </Button>
