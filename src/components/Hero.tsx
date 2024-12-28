c5531dd728b541a2932efb972f606d8d7cd8656a
dliu04 <dliuninja@gmail.com>
2024-12-28 14:28:17 -0800

    feat Hero Video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,10 @@ import '../App.css'
 import { Button } from './Button.tsx'
 import './Hero.css'
 
-
 function Hero() {
   return (
     <div className='hero-container'>
-        {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
+        <video src={process.env.PUBLIC_URL + '/videos/evenly.mp4'} autoPlay loop muted />
         <h1>Evenly</h1>
         <h2>Bill splitting, done simply.</h2>
         <p>
@@ -15,7 +14,7 @@ function Hero() {
         </p>
         <div className='hero-btns'>
             <Button to='/docs' className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get Started</Button>
-            <Button to='/download' className='btns' buttonStyle='btn--download' buttonSize='btn--large'>Download <i className="fa fa-download" /> </Button>
+            <Button to='/download' className='btns' buttonStyle='btn--download' buttonSize='btn--large'>Download <i className="fa fa-download"/> </Button>
         </div>
     </div>
   );
