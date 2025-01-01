18e8972fee134654d96ddb8b47d642a2e1049770
dliu04 <dliuninja@gmail.com>
2024-12-31 16:03:21 -0800

    remove Video Player from Mobile Viewports

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import '../App.css';
 
 export default function Docs() {
-    return <h1 className="docs">Docs</h1>
+    return (
+        <h1 className="docs">Docs</h1>
+    );
 }
